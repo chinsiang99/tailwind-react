8215e904217c34e67b3d22dc07d8a8175f762e25
Tan Chin Siang <chinsiang99@gmail.com>
2024-05-09 14:51:36 +0800

    feat: create service section

--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,6 @@ import Nav from "./Nav"
 import ShoeCard from "./ShoeCard"
 import SideNav from "./SideNav"
 import PopularProductCard from "./PopularProductCard"
+import ServiceCard from "./ServiceCard"
 
-export {Button, Nav, ShoeCard, SideNav, PopularProductCard}
+export {Button, Nav, ShoeCard, SideNav, PopularProductCard, ServiceCard}
--- src/sections/Services.jsx
+++ src/sections/Services.jsx
@@ -1,9 +1,16 @@
 // import React from 'react'
 // we can no need to import as we are having a new react version
-
+import { services } from "../constants"
+import { ServiceCard } from "../components"
 const Services = () => {
   return (
-    <div>Services</div>
+    <section className="flex flex-wrap max-container justify-center gap-9">
+      {
+        services.map(service=>(
+          <ServiceCard key={service.label} {...service}/>
+        ))
+      }
+    </section>
   )
 }
 
