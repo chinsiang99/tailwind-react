6de59ac114d30d56e59b36b229f49d5ade3601d2
Tan Chin Siang <chinsiang99@gmail.com>
2024-05-09 12:28:34 +0800

    feat: create popular product section component

--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,6 @@ import Button from "./Button"
 import Nav from "./Nav"
 import ShoeCard from "./ShoeCard"
 import SideNav from "./SideNav"
+import PopularProductCard from "./PopularProductCard"
 
-export {Button, Nav, ShoeCard, SideNav}
+export {Button, Nav, ShoeCard, SideNav, PopularProductCard}
